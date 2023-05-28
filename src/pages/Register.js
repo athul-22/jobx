c7b1615815c8241c548346445939218c34709783
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-05-29 00:46:47 +0530

    1

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../components/Logo'
-import '../styles/register.css'
+import Wrapper from '../wrappers/RegisterPage'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+}
 
 function Register() {
-  return (
-    <div className='reg'>
-        <Logo/>
-        <h1>ss</h1>
-    </div>
-  )
+
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target)
+    }
+
+    return (
+        <Wrapper className="full-page" >
+            <form className='form' onSubmit={onSubmit}>
+           <center><Logo /></center> 
+            <h3>Login</h3>
+            <div className='form-row'>
+                <label htmlFor='name' className='form-label'>
+                    Name: 
+                </label>
+                <input
+                type='text'
+                value={values.name}
+                name='name'
+                onChange={handleChange}
+                className='form-input'
+                />
+                <button className='btn btn-block'>SUBMIT</button>
+            </div>
+            </form>
+        </Wrapper>
+    )
 }
 
 export default Register
