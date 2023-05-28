cb775a65b46519ee4e5f372431c465f6f94bbc26
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-05-28 23:03:35 +0530

    1

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Logo from '../components/Logo'
+import Wrapper from '../wrappers/RegisterPage'
 
 function Register() {
   return (
-    <div>Register</div>
+    <Wrapper>
+        <h1>ss</h1>
+    </Wrapper>
   )
 }
 
