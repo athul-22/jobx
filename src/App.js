372833486a51ceadb05dd7a1677a603be1d336ee
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-05-28 21:04:25 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter,Routes, Route, Link } from "react-router-domnpm update";
 import Landing from "./pages/landing";
 
 function App() {
