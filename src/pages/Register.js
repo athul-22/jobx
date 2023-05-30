e5bd87d94748b6ae30e3abfbc2815c9566649b56
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-05-31 00:05:07 +0530

    1

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -29,13 +29,12 @@ function Register() {
 
     const handleChange = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value })
+        
     }
 
     const onSubmit = (e) => {
       
-        setValues.name(name);
-        setValues.email(email);
-        setValues.password(pass)
+        e.preventDefault();
         const { name, email, pass, isMember } = values;
         
         console.log(values)
@@ -48,12 +47,12 @@ function Register() {
                 <h3>{values.isMember ? 'Login' : 'Register'}</h3>
                 {showAlert && <Alert />}
                 {!values.isMember && (
-                    <FormRow type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+                    <FormRow type="text" name="name" value={values.name} onChange={handleChange} />
                 )}
                 <FormRow type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <FormRow type="password" name="password" value={pass} onChange={(e) => setPass(e.target.value)} />
 
-                <button className='btn btn-block'>SUBMIT</button>
+                <button className='btn btn-block' onClick={onSubmit}>SUBMIT</button>
                 <p> {values.isMember ? "Don't have an account?" : "Already have an account?"}
                     <button type='button' onClick={toggleMember} className='member-btn'>
                         {values.isMember ? 'Register' : 'Login'}
